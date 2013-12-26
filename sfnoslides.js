1cb872914460cb5043e91a737dfc0ea33ca5dc24
Jarek Machaj <czemuja@gmail.com>
2013-12-26 18:02:38 +0100

    fixes
    works man!

--- sfnoslides.js
+++ sfnoslides.js
@@ -1,4 +1,4 @@
-//window.onload = function() { start(); }
+start();
 
 function start() {		
 		var condition = $('nav.fullsizedPaggedNavigation').length > 0;
@@ -10,9 +10,10 @@ function prepareCalls(urlsArr){
 	var calls = [];
 	for (var i = 0; i < urlsArr.length; i++){
 		(function(i) {
+				console.log('calls push:' + i)
 				calls.push(
 					$.ajax({ url: urlsArr[i], type: 'get', dataType: 'html',  async: true 
-					}).success(function(data){ console.log('success') }));
+					}).success(function(data){ console.log('success:' + i) }));
   		})(i);		
 	}
 	return calls;
@@ -27,7 +28,10 @@ function getPagesUrls(){
 		if (splitted.length > 6)
 			splitted.pop();
 
-		return splitted.join('/');
+		var merged = splitted.join('/');
+		if (merged.slice(-1) == '#')
+			merged = merged.slice(0, -1);
+		return merged;
 	}
 
 	pageCount = $('nav.fullsizedPaggedNavigation span').html() == null ? 0 : $('nav.fullsizedPaggedNavigation span').html().split('/')[1];
@@ -36,6 +40,7 @@ function getPagesUrls(){
 	for (var i = 1; i < pageCount; i++) {
 		pagesAddresses[i] = getRootUrl() + '/' + (i+1); 
 	}
+	console.log(pagesAddresses);
 	return pagesAddresses;
 }
 
@@ -44,18 +49,21 @@ deferreds = null;
 function loadExtension(){
 	console.log("loading");
 	$('nav.fullsizedPaggedNavigation').append('<div><a id="loadAllButton" href="#">Ładuj</a></div>');
-		var responses = [];
+	$('#loadAllButton').click(function(){
 		deferreds = prepareCalls(getPagesUrls());
-		$('.box-article').html("");
         $.when.apply(null, deferreds).done(function() {
         	console.log('----done-----');
+        	var full = $('<div></div>');
         	for (var i = 0; i < deferreds.length; i++){
-        		console.log('processing ' + i);
-        		$('.box-article').append(processContent(i, $.parseHTML(deferreds[i].responseText)));
+        		console.log('processing ' + i);        		
+        		full.append(processContent(i, $.parseHTML(deferreds[i].responseText)));
+        		console.log('response length:' + deferreds[i].responseText.length);
         	}
+        	$('.box-article').html("");
+        	$('.box-article').append(full);
         });
+	});
 }
-
 
 function processContent(order, contentDoc){
 	var content = $("<div></div>");
@@ -64,5 +72,9 @@ function processContent(order, contentDoc){
 		content.append($(contentDoc).find('header.article-header')[0]);		
 		content.append($('<div style="margin-top: 50px;"></div>').append($(contentDoc).find('div.article-lead')[0]));
 	}
+	var art = $(contentDoc).find('article.box-article');
+	art.find('footer.article-footer, header.article-header, div.article-lead, nav.fullsizedPaggedNavigation, form.contentpoll').remove();
+	content.append(art);
+	//$(document).find('article.box-article').find('footer.article-footer, header.article-header, div.article-lead, nav.fullsizedPaggedNavigation, form.contentpoll').remove()
 	return content;
 }
